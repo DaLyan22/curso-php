5cad903b19f931eebaf706a539add6d4fe2bb299
DaLyan22 <gabrielgomessoares2007@gmail.com>
2023-05-20 01:03:57 -0300

    CURSO PHP

---
+++ comentarios-php/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+ <!-- comentário html -->
+
+    <?php
+
+    //comentário de linha única
+
+    #comententário de linha única
+
+    /* comentário de 
+                 várias linhas*/
+
+
+    echo "teste de terminal" . PHP_EOL; #"PHP_END-OF-LINE"
+    print "Novo teste!";
+
+#comentários php não aparecem no código fonte
+
+
+    ?>
+
+
+
+</body>
+
+</html>
